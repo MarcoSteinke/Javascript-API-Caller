d2df4f75c6d190880a6942b89874611e92e64ae9
maistai <49523050+maste150hhu@users.noreply.github.com>
2020-04-23 23:11:40 +0200

    add method to send a request and display result in the response-textarea

--- js/APIRequestService.js
+++ js/APIRequestService.js
@@ -2,6 +2,14 @@ class APIRequestService {
 
     constructor() {
         console.log("APIRequestService initialized");
+        this.textarea = document.querySelector("#api-response");
+    }
 
+    requestAPI(url, params) {
+        fetch(url)
+            .then(result => result.json())
+            .then(data => {
+                this.textarea.innerHTML = JSON.stringify(data);
+            });
     }
 }
